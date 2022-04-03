ddd07035ec1481264e0a9b03fe905290a7f339bc
Rae <102484944+just-rae@users.noreply.github.com>
2022-04-03 09:42:18 +0200

    Update rock,paper,scissors.py

--- rock,paper,scissors.py
+++ rock,paper,scissors.py
@@ -3,25 +3,16 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
         or (player == 'p' and opponent == 'r') or (player=='r'and opponent =='l')or (player=='l'and opponent=='sp')or (player=='sp'and opponent=='s')or (player=='s'and opponent=='l')or (player=='sp'and opponent=='r')or(player=='l'and opponent=='p')or(player=='p'and opponent=='sp'):
         return True
-def play():
-    counter=0
-    while True:
-        user = input("What's your choice? 'r' for rock, 'p' for paper, 'l' for lizard ,'sp' for spock,'s' for scissors\n. Press q to quit ").lower()
-        computer = random.choice(['r', 'p', 's'])
-        if user == computer:
-            return 'It\'s a tie'
-        if is_win(user, computer):
-            return "Whoo..you won! Number of games won:"+counter+=1
-        if user=="q":
-            return counter
-        if is_win(computer,user):
-            return 'You lost this game:('
-choice=input("Type yes if u wanna play rock paper scissors lizard spock or type q to quit. ").lower()
+counter=0
 while True:
-    if choice=="yes":
-        print(play())
-    elif choice=="q":
-        break 
-    else:
-        print("Response is invalid.")
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 'l' for lizard ,'sp' for spock,'s' for scissors\n. Press q to quit ").lower()
+    computer = random.choice(['r', 'p', 's'])
+    if user == computer:
+        print ('It\'s a tie')
+    if user=="q":
+        print (counter)
+    if is_win(user,computer):
+        counter=counter+1
+        print( "Whoo..you won!")
+    print( 'Computer won this game:(')
     
