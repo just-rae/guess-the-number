aafdf6ee1c5d17c3a5f1e2a0e092f997bbbc955a
Rae <102484944+just-rae@users.noreply.github.com>
2022-05-12 04:27:03 +0200

    Update IMAGEMANIPULATIONNN.py

--- IMAGEMANIPULATIONNN.py
+++ IMAGEMANIPULATIONNN.py
@@ -16,28 +16,40 @@ while True:
     choice=input("How would u like to modify ur image(resize,png,black/white,blur,enhance,or rotate) type random character to quit").lower()
     #rotates the user's chosen image and shows changes
     if choice=="rotate":
-        image1=Image.open(user_choice + ".jpg")
-        rot_num=int(input("Length of rotation?: "))
-        image1.rotate(rot_num).save(user_choice+'_rotate.jpg')
-        image2= Image.open(user_choice+'_rotate.jpg')
-        image2.show()
+        #try and except statement to prevent potential errors from the user input
+        try:
+            image1=Image.open(user_choice + ".jpg")
+            rot_num=int(input("Length of rotation?: "))
+            image1.rotate(rot_num).save(user_choice+'_rotate.jpg')
+            image2= Image.open(user_choice+'_rotate.jpg')
+            image2.show()
+        except:
+            print("input must be an integer")
 
 
     #resizes user's chosen image and shows changes
     elif choice=="resize":
-        image1=Image.open(user_choice + ".jpg")
-        choose=int(input("choose 1-3 (size_200='1'/size_400='2'/size_600='3'): "))
-        image1.resize(sizes[choose]).save(user_choice+'_resize.jpg')
-        image2= Image.open(user_choice+'_resize.jpg')
-        image2.show()
+        #try and except statement to prevent potential errors from the user input
+        try:
+            image1=Image.open(user_choice + ".jpg")
+            choose=int(input("choose 1-3 (size_200='1'/size_400='2'/size_600='3'): "))
+            image1.resize(sizes[choose]).save(user_choice+'_resize.jpg')
+            image2= Image.open(user_choice+'_resize.jpg')
+            image2.show()
+        except:
+            print("you must choose a number between 0 and 4")
 
      #blurs user's chosen image and shows changes        
     elif choice=="blur":
-        image1=Image.open(user_choice + ".jpg")
-        u_choice=int(input("how much do u want to blur it? "))
-        image1.filter(ImageFilter.GaussianBlur(u_choice)).save(user_choice+'_blur.jpg')
-        image2= Image.open(user_choice+'_blur.jpg')
-        image2.show()
+        #try and except statement to prevent potential errors from the user input
+        try:
+            image1=Image.open(user_choice + ".jpg")
+            u_choice=int(input("how much do u want to blur it? "))
+            image1.filter(ImageFilter.GaussianBlur(u_choice)).save(user_choice+'_blur.jpg')
+            image2= Image.open(user_choice+'_blur.jpg')
+            image2.show()
+        except:
+            print("input must be an integer")
     #changes picture to b&w and shows changes
     elif choice=="black/white":
         image1=Image.open(user_choice + '.jpg')
@@ -53,13 +65,17 @@ while True:
 
     #darkens or brightens the picture and shows changes
     elif choice=="enhance":
-        image1=Image.open(user_choice + ".jpg")
-        enhancer = ImageEnhance.Brightness(image1) 
-        brightness_choice=float(input("how dark or bright do you want it to be?(0.5-dark/1.5-bright)"))
-        img_light = enhancer.enhance(brightness_choice) 
-        img_light.save(user_choice+'_enhanced.jpg')
-        image2= Image.open(user_choice+'_enhanced.jpg')
-        image2.show()
+        #try and except statement to prevent potential errors from the user input
+        try:
+            image1=Image.open(user_choice + ".jpg")
+            enhancer = ImageEnhance.Brightness(image1) 
+            brightness_choice=float(input("how dark or bright do you want it to be?(0.5-dark/1.5-bright)"))
+            img_light = enhancer.enhance(brightness_choice) 
+            img_light.save(user_choice+'_enhanced.jpg')
+            image2= Image.open(user_choice+'_enhanced.jpg')
+            image2.show()
+         except:
+            print("input must be an integer or a decimal")
 
     else:
         print("invalid response.")
